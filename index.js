226019b972f911887574dea5aa59e83d632113ca
Kris Walker <kris@kixx.name>
2017-10-25 14:57:59 -0400

    Bug fixes to run our first program!
     Changes to be committed:
    	renamed:    lib/index.js -> index.js
    	modified:   lib/classes/component-registry.js
    	modified:   lib/create-process-manager.js
    	modified:   lib/define-function.js
    	deleted:    lib/library.js
    	new file:   library.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 exports.ProgrammerError = require(`./lib/classes/programmer-error`);
 exports.FrameworkError = require(`./lib/classes/framework-error`);
 
-exports.Library = require(`./lib/library`);
-
 exports.composeMiddleware = require(`./lib/compose-middleware`);
 exports.createProcessManager = require(`./lib/create-process-manager`);
 exports.defineFunction = require(`./lib/define-function`);
---
+++ library.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const KixxAssert = require(`kixx-assert`);
+
+Object.assign(exports, KixxAssert.helpers);
+
+exports.assert = KixxAssert.assert;
+
+exports.append = function append(item, list) {
+	list = list.slice();
+	list.push(item);
+	return list;
+};
+
+exports.assoc = function assoc(key, value, hash) {
+	hash = Object.assign({}, hash);
+	hash[key] = value;
+	return hash;
+};
+
+exports.deepFreeze = function deepFreeze(obj) {
+	Object.freeze(obj);
+	Object.getOwnPropertyNames(obj).forEach((key) => {
+		if (typeof obj === `function` &&
+			(key === `arguments` || key === `caller` || key === `callee` || key === `prototype`)) {
+			return;
+		}
+
+		const prop = obj[key];
+		if (prop !== null && (typeof prop === `object` || typeof prop === `function`)) {
+			deepFreeze(prop);
+		}
+	});
+
+	return obj;
+};
