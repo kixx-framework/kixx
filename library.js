3f8649cb5d4b51b75a4132541d26bb5b9b342982
Kris Walker <kris@kixx.name>
2017-11-07 06:38:56 -0500

    Store middleware.

--- library.js
+++ library.js
@@ -2,6 +2,8 @@
 
 const KixxAssert = require(`kixx-assert`);
 
+const {isObject, isPrimitive, isUndefined} = KixxAssert.helpers;
+
 Object.assign(exports, KixxAssert.helpers);
 
 exports.assert = KixxAssert.assert;
@@ -21,6 +23,36 @@ exports.assoc = function assoc(key, value, hash) {
 	hash[key] = value;
 	return hash;
 };
+
+exports.merge = function merge(target, ...sources) {
+	return sources.reduce((target, source) => {
+		if (isUndefined(source)) {
+			source = Object.create(null);
+		} else if (isPrimitive(source)) {
+			source = Object.assign(Object.create(null), source);
+		}
+		return mergeObject(
+			Object.assign(Object.create(null), target),
+			source
+		);
+	}, target);
+};
+
+function mergeObject(a, b) {
+	return Object.keys(b).reduce((a, k) => {
+		const v = b[k];
+		if (Array.isArray(v)) {
+			a[k] = mergeObject([], v);
+		} else if (isObject(v) && isPrimitive(a[k])) {
+			a[k] = mergeObject(Object.assign(Object.create(null), a[k]), v);
+		} else if (isObject(v)) {
+			a[k] = mergeObject(Object.create(null), v);
+		} else if (isPrimitive(v)) {
+			a[k] = v;
+		}
+		return a;
+	}, a);
+}
 
 exports.deepFreeze = function deepFreeze(obj) {
 	Object.freeze(obj);
