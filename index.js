05e4d6b6b19251534865c8fc341123c3b9517cca
Kris Walker <kris@kixx.name>
2017-11-08 19:37:35 -0500

    New InvariantError class.
     Changes to be committed:
    	modified:   index.js
    	new file:   lib/classes/invariant-error.js
    	modified:   lib/store-middleware/scan-objects-by-type.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ exports.ProgrammerError = require(`./lib/classes/programmer-error`);
 exports.FrameworkError = require(`./lib/classes/framework-error`);
 exports.ConflictError = require(`./lib/classes/conflict-error`);
 exports.NotImplementedError = require(`./lib/classes/not-implemented-error`);
+exports.InvariantError = require(`./lib/classes/invariant-error`);
 
 exports.composeMiddleware = require(`./lib/compose-middleware`);
 exports.createProcessManager = require(`./lib/create-process-manager`);
