83def7f0ad0921064ae4fcd1c80e69d481ba0375
Kris Walker <kris@kixx.name>
2018-02-18 16:03:44 -0500

    Fix linting (detect missing Promise)
     Changes to be committed:
    	modified:   .eslintrc.js
    	modified:   lib/initialize-components.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
+	"extends": "eslint:recommended",
 	"parserOptions": {
 		"ecmaVersion": 2017
 	},
 	"env": {
-		"es6": true,
 		"node": true
 	},
-	"extends": "eslint:recommended",
+	"globals": {
+		"Symbol": true
+	},
 	"rules": {
 		"strict": [
 			"error"
