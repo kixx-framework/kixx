7be359799ec35c01e0e49e3d44867e712c47e6aa
Kris Walker <kris@kixx.name>
2018-04-04 08:06:53 -0400

    Remove deprecated files.
     Changes to be committed:
    	deleted:    config.js
    	modified:   index.js
    	modified:   lib/classes/application-interface.js
    	deleted:    lib/classes/command-bus.js
    	deleted:    lib/classes/component-registry.js
    	deleted:    lib/classes/event-bus.js
    	deleted:    lib/classes/store-args.js
    	deleted:    lib/compose-express-controller.js
    	deleted:    lib/compose-link.js
    	modified:   lib/compose-middleware.js
    	deleted:    lib/create-application-store.js
    	deleted:    lib/create-process-manager.js
    	deleted:    lib/define-function.js
    	deleted:    lib/express-middleware/accept-json-api.js
    	deleted:    lib/express-middleware/allowed-methods.js
    	deleted:    lib/express-middleware/authenticate.js
    	deleted:    lib/express-middleware/authorize.js
    	deleted:    lib/express-middleware/body-to-json-api.js
    	deleted:    lib/express-middleware/collection-create.js
    	deleted:    lib/express-middleware/collection-list.js
    	deleted:    lib/express-middleware/create-transaction.js
    	deleted:    lib/express-middleware/cross-origin-request.js
    	deleted:    lib/express-middleware/default-error-handler.js
    	deleted:    lib/express-middleware/dispatch-method.js
    	deleted:    lib/express-middleware/handle-json-api-error.js
    	deleted:    lib/express-middleware/handle-not-found.js
    	deleted:    lib/express-middleware/parse-body.js
    	deleted:    lib/express-middleware/relationships-append.js
    	deleted:    lib/express-middleware/relationships-list.js
    	deleted:    lib/express-middleware/relationships-remove.js
    	deleted:    lib/express-middleware/relationships-replace.js
    	deleted:    lib/express-middleware/request-options.js
    	deleted:    lib/express-middleware/resource-get.js
    	deleted:    lib/express-middleware/resource-remove.js
    	deleted:    lib/express-middleware/resource-update.js
    	deleted:    lib/express-middleware/send-json-api.js
    	deleted:    lib/express-middleware/validate-json-api-relationships-request.js
    	deleted:    lib/express-middleware/validate-json-api-resource-request.js
    	modified:   lib/initialize-components.js
    	deleted:    lib/merge-store-records.js
    	modified:   lib/run-task.js
    	deleted:    lib/store-middleware/check-create-conflict.js
    	deleted:    lib/store-middleware/emit.js
    	deleted:    lib/store-middleware/generate-id.js
    	deleted:    lib/store-middleware/get-object.js
    	deleted:    lib/store-middleware/merge-object.js
    	deleted:    lib/store-middleware/no-op.js
    	deleted:    lib/store-middleware/not-implemented.js
    	deleted:    lib/store-middleware/remove-object.js
    	deleted:    lib/store-middleware/scan-objects-by-type.js
    	deleted:    lib/store-middleware/set-object.js
    	deleted:    lib/store-middleware/validate-object-exists.js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-// TODO: Add cache control middleware.
-// TODO: Add cookie parsing middleware.
-// TODO: Add set cookie middleware.
-// TODO: Add log request response middleware.
-// TODO: Add redirect middleware.
-// TODO: Add render template middleware.
-// TODO: Add authorizationOutput middleware to filter and/or decorate resource and included data.
-
 exports.BadRequestError = require(`./lib/classes/bad-request-error`);
 exports.ConflictError = require(`./lib/classes/conflict-error`);
 exports.ForbiddenError = require(`./lib/classes/forbidden-error`);
@@ -25,18 +17,10 @@ exports.UnsupportedMediaTypeError = require(`./lib/classes/unsupported-media-typ
 exports.UserError = require(`./lib/classes/user-error`);
 
 exports.ApplicationInterface = require(`./lib/classes/application-interface`);
-exports.EventBus = require(`./lib/classes/event-bus`);
-exports.CommandBus = require(`./lib/classes/command-bus`);
 exports.Logger = require(`./lib/classes/logger`);
-exports.StoreArgs = require(`./lib/classes/store-args`);
 
-exports.composeLink = require(`./lib/compose-link`);
-exports.composeMiddleware = require(`./lib/compose-middleware`);
 exports.computeObjectHash = require(`./lib/compute-object-hash`);
-exports.createApplicationStore = require(`./lib/create-application-store`);
 exports.createJsonWebToken = require(`./lib/create-json-web-token`);
-exports.createProcessManager = require(`./lib/create-process-manager`);
-exports.defineFunction = require(`./lib/define-function`);
 exports.httpFetchBuffer = require(`./lib/http-fetch-buffer`);
 exports.httpSendBuffer = require(`./lib/http-send-buffer`);
 exports.initializeComponents = require(`./lib/initialize-components`);
@@ -48,57 +32,3 @@ exports.uuidV1 = require(`uuid/v1`);
 exports.uuidV3 = require(`uuid/v3`);
 exports.uuidV4 = require(`uuid/v4`);
 exports.uuidV5 = require(`uuid/v5`);
-
-const ExpressMiddleware = [
-	`accept-json-api`,
-	`allowed-methods`,
-	`authenticate`,
-	`authorize`,
-	`body-to-json-api`,
-	`collection-create`,
-	`collection-list`,
-	`create-transaction`,
-	`cross-origin-request`,
-	`default-error-handler`,
-	`dispatch-method`,
-	`handle-json-api-error`,
-	`handle-not-found`,
-	`parse-body`,
-	`relationships-append`,
-	`relationships-list`,
-	`relationships-remove`,
-	`relationships-replace`,
-	`request-options`,
-	`resource-get`,
-	`resource-remove`,
-	`resource-update`,
-	`send-json-api`,
-	`validate-json-api-relationships-request`,
-	`validate-json-api-resource-request`
-];
-
-exports.ExpressMiddleware = Object.freeze(ExpressMiddleware.reduce((middleware, filename) => {
-	const fn = require(`./lib/express-middleware/${filename}`);
-	middleware[fn.name] = fn;
-	return middleware;
-}, Object.create(null)));
-
-const StoreMiddleware = [
-	`check-create-conflict`,
-	`emit`,
-	`generate-id`,
-	`get-object`,
-	`merge-object`,
-	`no-op`,
-	`not-implemented`,
-	`remove-object`,
-	`scan-objects-by-type`,
-	`set-object`,
-	`validate-object-exists`
-];
-
-exports.StoreMiddleware = Object.freeze(StoreMiddleware.reduce((middleware, filename) => {
-	const fn = require(`./lib/store-middleware/${filename}`);
-	middleware[fn.name] = fn;
-	return middleware;
-}, Object.create(null)));
