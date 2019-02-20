01a5b6a456c1b1da3f86e7acb501a508e2b55df8
Kris Walker <kris@kixx.name>
2019-02-19 19:28:13 -0500

    Running Task happy path test.

--- test/lib/atypes/task-test.js
+++ test/lib/atypes/task-test.js
@@ -1,18 +1,35 @@
 'use strict';
 
 const { assert } = require('kixx-assert');
+// const pureTest = require('../../../lib/pure-test');
 
-const pureTest = require('../../../lib/pure-test');
+const Task = require('../../../lib/atypes/task');
 
 
 module.exports = (test) => {
-	pureTest(test, 'happy path', (t) => {
-		t.before(() => {
-			return 9;
+	test.describe('happy path', (t) => {
+		const DELAYED_RESULT = Object.freeze({
+			DELAYED_RESULT: true
 		});
 
-		t.it('is not smoking', (result) => {
-			assert.isEqual(9, result);
+		let result;
+
+		t.before((done) => {
+			const f = new Task((reject, resolve) => {
+				setTimeout(() => {
+					resolve(DELAYED_RESULT);
+				}, 10);
+			});
+
+			f.fork(done, (x) => {
+				result = x;
+				done();
+			});
+		});
+
+		t.it('is not smoking', () => {
+			assert.isNotEmpty(result);
+			assert.isEqual(DELAYED_RESULT, result);
 		});
 	});
 };
