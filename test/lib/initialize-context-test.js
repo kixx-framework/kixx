3e073d870f1a56176e8b5ea329770518c8157f5c
Kris Walker <kris@kixx.name>
2019-02-26 22:22:26 -0500

    Start of testing for refactored initializer.

--- test/lib/initialize-context-test.js
+++ test/lib/initialize-context-test.js
@@ -1,14 +1,18 @@
 'use strict';
 
 const { assert } = require('kixx-assert');
+const sinon = require('sinon');
 const R = require('ramda');
+
+const Task = require('../../lib/atypes/task');
+const U = require('../../lib/utils');
 
 const {
 	component,
 	initializer,
 	initializeContext,
-	//contextReducer,
-	//flattenAndSerializeComponents
+	// contextReducer,
+	// flattenAndSerializeComponents
 } = require('../../lib/initialize-context');
 
 
@@ -80,6 +84,120 @@ APIContext.uid = uid();
 
 
 module.exports = (test) => {
+	test.describe('component()', (t) => {
+		t.it('passes eq', () => {
+			const a = 2;
+			const b = 3;
+			const c = 4;
+
+			// Perform some random operation.
+			const fn = (a, b, c) => a * b + c;
+
+			const c1 = fn(a, b, c);
+
+			assert.isNotEqual(c, c1);
+			assert.isEqual(10, c1);
+
+			// Take advantage of curried component() function.
+			const createComponent = component(fn);
+
+			const [ a1, b1, c2 ] = createComponent(a, b, c);
+
+			assert.isEqual(a, a1);
+			assert.isEqual(b, b1);
+			assert.isNotEqual(c, c2);
+
+			assert.isEqual(c1, c2);
+		});
+
+		t.it('calls the wrapper and uses the response', () => {
+			const a = 2;
+			const b = 3;
+			const c = 4;
+
+			const wrapper = sinon.fake((a, b, c) => a * b + c);
+
+			// Take advantage of curried component() function.
+			const createComponent = component(wrapper);
+
+			const [ a1, b1, c1 ] = createComponent(a, b, c);
+
+			assert.isEqual(a, a1);
+			assert.isEqual(b, b1);
+			assert.isNotEqual(c, c1);
+			assert.isEqual(10, c1);
+
+			assert.isEqual(1, wrapper.callCount);
+
+			const { args } = wrapper.firstCall;
+			assert.isEqual(a, args[0]);
+			assert.isEqual(b, args[1]);
+			assert.isEqual(c, args[2]);
+		});
+	});
+
+	test.describe('initialize()', (t) => {
+		t.it('invokes fn', () => {
+			const a = 1;
+			const b = 2;
+			const c = 3;
+
+			const fn = sinon.fake((x) => x * 2);
+
+			const init = initializer(a, b, fn);
+
+			const t = init(c);
+
+			t.fork(
+				(err) => assert.isOk(false, err.message),
+				([ c1, y ]) => {
+					assert.isEqual(c, c1);
+					assert.isEqual(6, y);
+				}
+			);
+
+			assert.isEqual(1, fn.callCount);
+		});
+	});
+
+	test.describe('initializer() when fn returns Task', (t) => {
+		t.it('returns an unnested Task', () => {
+			const c = 1;
+
+			const fn = () => Task.of(9);
+
+			const init = initializer(U._, U._, fn);
+
+			const t = init(c);
+
+			t.fork(
+				(err) => assert.isOk(false, err.message),
+				([ c1, y ]) => {
+					assert.isEqual(c, c1);
+					assert.isEqual(9, y);
+				}
+			);
+		});
+	});
+
+	test.describe('initializer() when fn throws', (t) => {
+		t.it('returns rejected Task', () => {
+			const ERR = new Error('TEST');
+
+			const init = initializer(U._, U._, () => {
+				throw ERR;
+			});
+
+			const t = init(null);
+			assert.isOk(Task.isTask(t));
+
+			t.fork(
+				(err) => assert.isEqual(ERR, err),
+				() => assert.isOk(false, 'unexpected path')
+			);
+		});
+	});
+
 	test.xdescribe('happy path', (t) => {
 		const CONFIGS = [
 			[ 'jan', [ 'mar' ] ],
@@ -144,7 +262,7 @@ module.exports = (test) => {
 		});
 	});
 
-	test.xdescribe('when component intializer returns non Task', (t) => {
+	test.xdescribe('when component initializer returns non Task', (t) => {
 		const X = Object.create(null);
 
 		const CONFIGS = [
