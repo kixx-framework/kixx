662814e0d22d6dd5436aaa78420b5191eae5542b
Kris Walker <kristoffer.walker@disneystreaming.com>
2023-02-18 06:12:25 -0500

    Making progress on full MVC architecture

--- index.mjs
+++ index.mjs
@@ -1,3 +1,9 @@
 import createApplicationServer from './lib/create-application-server.mjs';
+import createAppRouter from './lib/components/create-app-router.mjs';
+import createTemplatePageHandler from './lib/components/create-template-page-handler.mjs';
 
-export default { createApplicationServer };
+export default {
+	createApplicationServer,
+	createAppRouter,
+	createTemplatePageHandler,
+};
