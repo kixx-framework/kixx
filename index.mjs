2fc8d0179907e2f508828ced99403d85189c8bd5
Kris Walker <kristoffer.walker@disneystreaming.com>
2023-02-19 04:51:06 -0500

    Start of new frameworks for serving templated pages

--- index.mjs
+++ index.mjs
@@ -1,9 +1,13 @@
 import createApplicationServer from './lib/create-application-server.mjs';
 import createAppRouter from './lib/components/create-app-router.mjs';
+import createLocalFileRecordStore from './lib/components/create-local-file-record-store.mjs';
 import createTemplatePageHandler from './lib/components/create-template-page-handler.mjs';
+import createLocalFileTemplateStore from './lib/components/create-local-file-template-store.mjs';
 
 export default {
 	createApplicationServer,
 	createAppRouter,
+	createLocalFileRecordStore,
+	createLocalFileTemplateStore,
 	createTemplatePageHandler,
 };
