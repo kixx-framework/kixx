9bb97a4a682161a20133fab1340ff4fc36a184f3
Kris Walker <kristoffer.walker@disneystreaming.com>
2023-02-20 05:34:39 -0500

    Refactor for including types in storage engines
     Changes to be committed:
    	modified:   index.mjs
    	modified:   lib/components/record-store.mjs
    	renamed:    lib/entities/base-entity.mjs -> lib/entity-types/base-entity-type.mjs
    	new file:   lib/entity-types/page-image.mjs
    	renamed:    lib/entities/page.mjs -> lib/entity-types/page.mjs
    	renamed:    lib/components/template-page-handler.mjs -> lib/request-handlers/template-page-handler.mjs
    	modified:   lib/storage-engines/local-file-record-store.mjs

--- index.mjs
+++ index.mjs
@@ -1,13 +1,36 @@
 import createApplicationServer from './lib/create-application-server.mjs';
-import createAppRouter from './lib/components/create-app-router.mjs';
-import createLocalFileRecordStore from './lib/components/create-local-file-record-store.mjs';
-import createTemplatePageHandler from './lib/components/create-template-page-handler.mjs';
-import createLocalFileTemplateStore from './lib/components/create-local-file-template-store.mjs';
+
+import { createLocalFileRecordStore } from './lib/storage-engines/local-file-record-store.mjs';
+import { createHandlebarsTemplateEngine } from './lib/template-engines/handlebars-template-engine.mjs';
+
+import { createRecordStore } from './lib/components/record-store.mjs';
+import { createTemplateStore } from './lib/components/template-store.mjs';
+import { createAppRouter } from './lib/components/app-router.mjs';
+
+import { createTemplatePageHandler } from './lib/request-handlers/template-page-handler.mjs';
+
+import BaseEntityType from './lib/entity-types/base-entity-type.mjs';
+import Page from './lib/entity-types/page.mjs';
+import PageImage from './lib/entity-types/page-image.mjs';
 
 export default {
 	createApplicationServer,
 	createAppRouter,
-	createLocalFileRecordStore,
-	createLocalFileTemplateStore,
-	createTemplatePageHandler,
+	createTemplateStore,
+	createRecordStore,
+
+	storageEngines: {
+		createLocalFileRecordStore,
+	},
+	templateEngines: {
+		createHandlebarsTemplateEngine,
+	},
+	requestHandlers: {
+		createTemplatePageHandler,
+	},
+	entityTypes: {
+		BaseEntityType,
+		Page,
+		PageImage,
+	},
 };
