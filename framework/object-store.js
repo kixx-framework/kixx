cf0211b05cdd327565e33897a55269468a2fef54
Kris Walker <kristoffer.walker@disneystreaming.com>
2023-03-05 14:28:18 -0500

    New ObjectStore
     Changes to be committed:
    	new file:   framework/object-store.js
    	new file:   framework/stores/local-file-object-store.js

---
+++ framework/object-store.js
@@ -0,0 +1,76 @@
+// @ts-check
+
+import { ProgrammerError, NotFoundError } from 'kixx-server-errors';
+import KixxAssert from 'kixx-assert';
+import { v4 as uuidv4 } from 'uuid';
+
+const { isNonEmptyString } = KixxAssert.helpers;
+
+
+export default class ObjectStore {
+    scope;
+    storageEngine;
+
+    /**
+     * @param {{scope:String,storageEngine:Object}} spec
+     */
+    constructor(spec) {
+        Object.defineProperties(this, {
+            scope: {
+                enumerable: true,
+                value: spec.scope,
+            },
+            storageEngine: {
+                enumerable: true,
+                value: spec.storageEngine,
+            },
+        });
+    }
+
+    initialize() {
+        return this.storageEngine.initialize().then(() => {
+            return this;
+        });
+    }
+
+    async readObject(id) {
+        if (!isNonEmptyString(id)) {
+            throw new ProgrammerError(
+                'The .readObject() "id" parameter must be a non empty string.'
+            );
+        }
+
+        const [ metadata, readStream ] = await this.storageEngine.readObject(id);
+
+        if (!metadata) {
+            throw new NotFoundError(
+                `Requested object ${ id } could not be found`
+            );
+        }
+
+        return [ metadata, readStream ];
+    }
+
+    async writeObject(metadata, readStream) {
+
+        if (!isNonEmptyString(metadata.contentType)) {
+            throw new ProgrammerError(
+                'The .writeObject() "metadata.contentType" must be a non empty string.'
+            );
+        }
+
+        const isoDateString = new Date().toISOString();
+
+        const record = Object.assign({}, metadata, {
+            scope: this.scope,
+            type: 'object',
+            id: uuidv4(),
+            created: isoDateString,
+            updated: isoDateString,
+        });
+
+        await this.storageEngine.writeObject(record, readStream);
+
+        return record;
+    }
+}
---
+++ framework/stores/local-file-object-store.js
@@ -0,0 +1,168 @@
+// @ts-check
+
+import fs from 'node:fs';
+import path from 'node:path';
+import { OperationalError } from 'kixx-server-errors';
+// Imported for type checking
+// eslint-disable-next-line no-unused-vars
+import EventBus from '../lib/event-bus.js';
+import { ErrorEvent } from '../lib/events.js';
+import { onStreamFinished } from '../lib/file-utils.js';
+
+export default class LocalFileObjectStore {
+
+    #eventBus;
+    #directory;
+
+    /**
+     * @param {{eventBus:EventBus,directory:String}} spec
+     */
+    constructor(spec) {
+        this.#eventBus = spec.eventBus;
+        this.#directory = spec.directory;
+    }
+
+    initialize() {
+        return new Promise((resolve, reject) => {
+            fs.mkdir(this.#directory, { recursive: true }, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(this);
+                }
+            });
+        });
+    }
+
+    writeObject(record, readStream) {
+        return new Promise((resolve, reject) => {
+            this.#writeObjectAndMetafile(record, readStream, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    readObject(id) {
+        return new Promise((resolve, reject) => {
+            this.#readMetafileAndObject(id, (err, [ metadata, readStream ]) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve([ metadata, readStream ]);
+                }
+            });
+        });
+    }
+
+    #writeObjectAndMetafile(record, readStream, callback) {
+        const filepath = this.#createObjectFilePath(record.id);
+
+        // TODO: Use the proper encoding for the file type.
+        // @ts-ignore error TS2322: Type 'null' is not assignable to type 'BufferEncoding | undefined'
+        const writeStream = fs.createWriteStream(filepath, { encoding: null });
+
+        readStream.on('error', (cause) => {
+            this.#emitError(new OperationalError(
+                'Encountered read stream error event while writing object',
+                { cause, info: { filepath } }
+            ), callback);
+        });
+
+        writeStream.on('error', (cause) => {
+            this.#emitError(new OperationalError(
+                'Encountered write stream error event while writing object',
+                { fatal: true, cause, info: { filepath } }
+            ), callback);
+        });
+
+        readStream.pipe(writeStream);
+
+        onStreamFinished(writeStream, () => {
+            writeStream.destroy();
+            this.#writeMetaFile(record, callback);
+        });
+    }
+
+    #readMetafileAndObject(id, callback) {
+        this.#readMetaFile(id, (err, metadata) => {
+            if (err) {
+                callback(err);
+                return;
+            }
+
+            if (!metadata) {
+                callback(null, [ null, null ]);
+                return;
+            }
+
+            const filepath = this.#createObjectFilePath(id);
+            const readStream = fs.createReadStream(filepath);
+
+            readStream.on('error', (cause) => {
+                this.#emitError(new OperationalError(
+                    'Encountered read stream error event while reading object',
+                    { fatal: true, cause, info: { filepath } }
+                ), callback);
+            });
+
+            onStreamFinished(readStream, () => {
+                readStream.destroy();
+            });
+
+            callback(null, [ metadata, readStream ]);
+        });
+    }
+
+    #writeMetaFile(record, callback) {
+        const { id } = record;
+        const utf8Data = JSON.stringify(record, null, 2);
+        const filepath = this.#createMetaRecordFilePath(id);
+
+        fs.writeFile(filepath, utf8Data, { encoding: 'utf8' }, (cause) => {
+            if (cause) {
+                callback(new OperationalError(
+                    'Encountered file write error while writing object metadata file',
+                    { fatal: true, cause, info: { filepath } }
+                ));
+            } else {
+                callback(null, record);
+            }
+        });
+    }
+
+    #readMetaFile(id, callback) {
+        const filepath = this.#createMetaRecordFilePath(id);
+
+        fs.readFile(filepath, { encoding: 'utf8' }, (cause, utf8Data) => {
+            if (cause) {
+                if (cause.code === 'ENOENT') {
+                    callback(null, null);
+                } else {
+                    callback(new OperationalError(
+                        'Encountered file read error while reading object metadata file',
+                        { fatal: true, cause, info: { filepath } }
+                    ));
+                }
+            } else {
+                callback(null, JSON.parse(utf8Data));
+            }
+        });
+    }
+
+    #emitError(error, callback) {
+        this.#eventBus.emitEvent(new ErrorEvent(error));
+        callback(error);
+    }
+
+    #createObjectFilePath(id) {
+        return path.join(this.#directory, id);
+    }
+
+    #createMetaRecordFilePath(id) {
+        return path.join(this.#directory, `${ id }.json`);
+    }
+}
