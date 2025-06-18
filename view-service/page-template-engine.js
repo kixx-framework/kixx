f488906b5bfee1fa1a5596c0e102e08e08745647
Kris Walker <kris@kixx.name>
2025-06-18 05:36:20 -0400

    Update the view service for the new template engine.
     On branch v3
     Changes to be committed:
    	modified:   view-service/page-template-engine.js

--- view-service/page-template-engine.js
+++ view-service/page-template-engine.js
@@ -7,18 +7,13 @@ import * as fileSystem from '../lib/file-system.js';
 
 export default class PageTemplateEngine {
 
-    #logger = null;
     #partialsDirectory = null;
     #fileSystem = null;
 
     constructor(options) {
-        this.#logger = options.logger;
         this.#partialsDirectory = options.partialsDirectory;
 
         this.#fileSystem = options.fileSystem || fileSystem;
-
-        this.verboseLogging = Boolean(options.verboseLogging);
-        this.renderErrorMessages = Boolean(options.renderErrorMessages);
 
         this.helpers = new Map(helpers);
         this.helpers.set('format_date', format_date);
@@ -48,81 +43,9 @@ export default class PageTemplateEngine {
     }
 
     compileTemplate(templateId, utf8) {
-        const tokens = this.tokenizeTemplateSource(templateId, utf8);
-        const tree = this.buildSyntaxTree(templateId, tokens);
-        return this.createRenderFunction(templateId, tree);
-    }
-
-    tokenizeTemplateSource(templateId, utf8) {
-        const { errors, tokens } = tokenize({}, templateId, utf8);
-
-        if (errors && errors.length > 0) {
-            if (this.verboseLogging) {
-                for (const error of errors) {
-                    this.#logger.warn('template parsing error', { templateId }, error);
-                    // eslint-disable-next-line no-console
-                    console.warn(error.toString());
-                }
-            }
-
-            throw errors[0];
-        }
-
-        return tokens;
-    }
-
-    buildSyntaxTree(templateId, tokens) {
-        const { errors, tree } = buildSyntaxTree({}, tokens);
-
-        if (errors && errors.length > 0) {
-            if (this.verboseLogging) {
-                for (const error of errors) {
-                    this.#logger.warn('template syntax error', { templateId }, error);
-                    // eslint-disable-next-line no-console
-                    console.warn(error.toString());
-                }
-            }
-
-            throw errors[0];
-        }
-
-        return tree;
-    }
-
-    createRenderFunction(templateId, tree) {
-        // Compile time errors will be returned here, but runtime errors will be
-        // handled according to the options and passed to the error handler.
-        const { errors, render } = createRenderFunction(
-            { includeErrorMessages: this.renderErrorMessages },
-            this.onRuntimeError.bind(this, templateId),
-            this.partials,
-            this.helpers,
-            tree
-        );
-
-        if (errors && errors.length > 0) {
-            if (this.verboseLogging) {
-                for (const error of errors) {
-                    this.#logger.warn('template create render function error', { templateId }, error);
-                    // eslint-disable-next-line no-console
-                    console.warn(error.toString());
-                }
-            }
-
-            throw errors[0];
-        }
-
-        return render;
-    }
-
-    onRuntimeError(templateId, { error, lineError }) {
-        if (this.verboseLogging) {
-            this.#logger.error('template runtime error', { templateId }, error);
-            // eslint-disable-next-line no-console
-            console.error(lineError.toString());
-        }
-
-        throw lineError;
+        const tokens = tokenize(null, templateId, utf8);
+        const tree = buildSyntaxTree(null, tokens);
+        return createRenderFunction(null, this.helpers, this.partials, tree);
     }
 
     filepathForTemplate(directory, templateId) {
