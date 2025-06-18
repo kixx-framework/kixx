0355deef0427651712f7ffef6cc7447293b5b8fe
Kris Walker <kris@kixx.name>
2025-06-18 07:39:22 -0400

    Remove the TemplateEngine export
     On branch v3
     Changes to be committed:
    	modified:   mod.js

--- mod.js
+++ mod.js
@@ -1,11 +1,9 @@
 import * as assertions from './assertions/mod.js';
 import * as errors from './errors/mod.js';
-import * as templateEngine from './template-engine/mod.js';
 import * as datastore from './datastore/mod.js';
 
 export {
     assertions,
     errors,
-    templateEngine,
     datastore
 };
