26a817ba26ba5c27780f510cf056711e17e790a9
Kris Walker <kris@kixx.name>
2025-07-20 12:23:08 -0400

    Update docs for the errors libary
     Changes to be committed:
    	modified:   errors/lib/assertion-error.js
    	modified:   errors/lib/bad-request-error.js
    	modified:   errors/lib/conflict-error.js
    	modified:   errors/lib/forbidden-error.js
    	modified:   errors/lib/method-not-allowed-error.js
    	modified:   errors/lib/not-acceptable-error.js
    	modified:   errors/lib/not-found-error.js
    	modified:   errors/lib/not-implemented-error.js
    	modified:   errors/lib/operational-error.js
    	modified:   errors/lib/unauthenticated-error.js
    	modified:   errors/lib/unauthorized-error.js
    	modified:   errors/lib/unsupported-media-type-error.js
    	modified:   errors/lib/validation-error.js
    	modified:   errors/lib/wrapped-error.js

--- errors/lib/assertion-error.js
+++ errors/lib/assertion-error.js
@@ -1,6 +1,34 @@
 import WrappedError from './wrapped-error.js';
 
 
+/**
+ * Represents an error thrown when an assertion fails.
+ * Typically used to indicate that an internal invariant or contract has been violated.
+ * Extends WrappedError to provide additional context and stack trace handling.
+ *
+ * @class
+ * @extends WrappedError
+ */
 export default class AssertionError extends WrappedError {
+    /**
+     * The error code string for this error type.
+     * @type {string}
+     * @static
+     * @readonly
+     */
     static CODE = 'ASSERTION_ERROR';
+
+    /**
+     * Constructs a new AssertionError instance.
+     *
+     * @param {string} message - The error message describing the failed assertion.
+     * @param {Object} [options] - Optional. Additional error options.
+     * @param {string} [options.code] - Optional custom error code.
+     * @param {string} [options.name] - Optional custom error name.
+     * @param {Error} [options.cause] - Optional cause of the error.
+     * @param {Function} [sourceFunction] - Optional. Function to use for stack trace capture.
+     */
+    constructor(message, options, sourceFunction) {
+        super(message, options, sourceFunction);
+    }
 }
