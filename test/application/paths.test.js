98c0a4c387699d56b2cb8e732344dd2445bb1563
Kris Walker <kris@kixx.name>
2025-08-09 18:59:53 -0400

    Update the Paths class and rewrite the tests for it.
     Changes to be committed:
    	modified:   lib/application/paths.js
    	new file:   test/application/paths.test.js

---
+++ test/application/paths.test.js
@@ -0,0 +1,68 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { describe } from 'kixx-test';
+import { assertEqual } from 'kixx-assert';
+import Paths from '../../lib/application/paths.js';
+
+const THIS_DIR = path.dirname(fileURLToPath(import.meta.url));
+const FAKE_APP_DIR = path.join(THIS_DIR, 'my-projects', 'fake-app');
+
+describe('Application/Paths#constructor with valid input', ({ before, it }) => {
+    let subject;
+
+    before(() => {
+        subject = new Paths(FAKE_APP_DIR);
+    });
+
+    it('should have the correct app_directory', () => {
+        assertEqual(subject.app_directory, FAKE_APP_DIR);
+    });
+
+    it('should have the correct routes_directory', () => {
+        assertEqual(subject.routes_directory, path.join(FAKE_APP_DIR, 'routes'));
+    });
+
+    it('should have the correct public_directory', () => {
+        assertEqual(subject.public_directory, path.join(FAKE_APP_DIR, 'public'));
+    });
+
+    it('should have the correct pages_directory', () => {
+        assertEqual(subject.pages_directory, path.join(FAKE_APP_DIR, 'pages'));
+    });
+
+    it('should have the correct templates_directory', () => {
+        assertEqual(subject.templates_directory, path.join(FAKE_APP_DIR, 'templates', 'templates'));
+    });
+
+    it('should have the correct helpers_directory', () => {
+        assertEqual(subject.helpers_directory, path.join(FAKE_APP_DIR, 'templates', 'helpers'));
+    });
+
+    it('should have the correct partials_directory', () => {
+        assertEqual(subject.partials_directory, path.join(FAKE_APP_DIR, 'templates', 'partials'));
+    });
+
+    it('should have the correct plugins_directory', () => {
+        assertEqual(subject.plugins_directory, path.join(FAKE_APP_DIR, 'plugins'));
+    });
+
+    it('should have the correct commands_directory', () => {
+        assertEqual(subject.commands_directory, path.join(FAKE_APP_DIR, 'commands'));
+    });
+
+    it('should have the correct data_directory', () => {
+        assertEqual(subject.data_directory, path.join(FAKE_APP_DIR, 'data'));
+    });
+
+    it('should have the correct kv_store_directory', () => {
+        assertEqual(subject.kv_store_directory, path.join(FAKE_APP_DIR, 'data', 'kv-store'));
+    });
+
+    it('should have the correct object_store_directory', () => {
+        assertEqual(subject.object_store_directory, path.join(FAKE_APP_DIR, 'data', 'objects'));
+    });
+
+    it('should have the correct job_directory', () => {
+        assertEqual(subject.job_directory, path.join(FAKE_APP_DIR, 'data', 'jobs'));
+    });
+});
