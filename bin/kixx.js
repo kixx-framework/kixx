0c554df1899b1a2b25388122b47daf361ef5dab5
Kris Walker <kris@kixx.name>
2025-09-12 09:45:02 -0400

    Start help documentation for command line
     Changes to be committed:
    	modified:   bin/kixx.js
    	new file:   cli/docs/valid-commands.md

--- bin/kixx.js
+++ bin/kixx.js
@@ -1,18 +1,27 @@
 #!/usr/bin/env node
 
+import { EOL } from 'node:os';
 import process from 'node:process';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import * as InitProject from '../cli/init-project.js';
 import * as AppServer from '../cli/app-server.js';
 import * as RunCommand from '../cli/run-command.js';
 
 
+const ROOT_DIR = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
+const DOCS_DIR = path.join(ROOT_DIR, 'cli', 'docs');
+
 const args = process.argv.slice(2);
 const commandName = args.shift();
 
+/* eslint-disable no-console */
+
 
 if (!commandName) {
-    // eslint-disable-next-line no-console
-    console.error('A command name is required');
+    console.error('Kixx requires a command name. Available commands are:' + EOL);
+    console.error(readDocFile('valid-commands.md'));
     process.exit(1);
 }
 
@@ -20,8 +29,11 @@ if (!commandName) {
 let promise;
 
 switch (commandName) {
-    case 'init-project':
-        promise = InitProject.main(args);
+    case 'help':
+        console.error('Run a Kixx command!' + EOL);
+        console.error('Available commands are:' + EOL);
+        console.error(readDocFile('valid-commands.md'));
+        promise = Promise.resolve();
         break;
     case 'app-server':
         promise = AppServer.main(args);
@@ -29,14 +41,22 @@ switch (commandName) {
     case 'run-command':
         promise = RunCommand.main(args);
         break;
+    case 'init-project':
+        promise = InitProject.main(args);
+        break;
     default:
-        // eslint-disable-next-line no-console
-        console.error(`The command "${ commandName }" is not recognized`);
+        console.error(`The Kixx command "${ commandName }" is not recognized. Available commands are:` + EOL);
+        console.error(readDocFile('valid-commands.md'));
         process.exit(1);
 }
 
 promise.catch((err) => {
-    // eslint-disable-next-line no-console
+    console.error(`There was an unexpected error while running "${ commandName }":` + EOL);
     console.error(err);
     process.exit(1);
 });
+
+function readDocFile(filename) {
+    const filepath = path.join(DOCS_DIR, filename);
+    return fs.readFileSync(filepath, { encoding: 'utf8' });
+}
---
+++ cli/docs/valid-commands.md
@@ -0,0 +1,29 @@
+help
+----
+Use the `help` command to print out long form documentation on the command line.
+
+Example: `npx kixx help`
+
+app-server
+----------
+The `app-server` command starts the application HTTP server.
+
+Example: `npx kixx app-server --port 3003`
+
+For more information, run `kixx app-server --help`.
+
+run-command
+-----------
+Use `run-command` to run one of your custom commands from your project.
+
+Example: `npx kixx run-command my-command --foo bar`
+
+For more information, run `kixx run-command --help`.
+
+init-project
+------------
+Use the `init-project` command to initialize a new Kixx project, or update the meta files for an existing project.
+
+Example: `npx kixx init-project --name "My Amazing Web App!"`
+
+For more information, run `kixx init-project --help`.
