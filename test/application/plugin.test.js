0a7864d33fa8ef89812df8a38dcec89b024c8135
Kris Walker <kris@kixx.name>
2025-10-08 09:23:48 -0400

    More plugin testing
     Changes to be committed:
    	modified:   lib/application/plugin.js
    	modified:   test/application/plugin.test.js

--- test/application/plugin.test.js
+++ test/application/plugin.test.js
@@ -11,6 +11,68 @@ import { assert, assertEqual } from 'kixx-assert';
 const DIRECTORY = path.dirname(fileURLToPath(import.meta.url));
 
 
+describe('Plugin#loadViews()', ({ before, after, it }) => {
+
+    class UserView { }
+
+    const readDirectory = sinon.stub().resolves([
+        {
+            name: 'user.view.mjs',
+            isDirectory() {
+                return false;
+            },
+        },
+        {
+            name: 'product',
+            isDirectory() {
+                return true;
+            },
+        },
+        {
+            name: 'user',
+            isDirectory() {
+                return true;
+            },
+        },
+        {
+            name: 'helpers.js',
+            isDirectory() {
+                return false;
+            },
+        },
+    ]);
+
+    const fileSystem = { readDirectory };
+
+    let plugin;
+    let result;
+
+    before(async () => {
+        plugin = new Plugin(fileSystem, DIRECTORY);
+
+        const loadView = sinon.stub(plugin, 'loadView');
+        loadView.onFirstCall().resolves(null);
+        loadView.onSecondCall().resolves(UserView);
+
+        result = await plugin.loadViews();
+    });
+
+    after(() => {
+        sinon.restore();
+    });
+
+    it('only loads nested directories', () => {
+        assertEqual(2, plugin.loadView.callCount);
+        assertEqual(path.join(DIRECTORY, 'views', 'product'), plugin.loadView.getCall(0).args[0]);
+        assertEqual(path.join(DIRECTORY, 'views', 'user'), plugin.loadView.getCall(1).args[0]);
+    });
+
+    it('uses the plugin name and ViewConstructor.name as the key', () => {
+        assertEqual(1, result.size);
+        assert(result.has('application.UserView'));
+    });
+});
+
 describe('Plugin#loadView()', ({ before, after, it }) => {
 
     class UserView { }
@@ -146,6 +208,68 @@ describe('Plugin#loadView() when class file does not exist', ({ before, after, i
     });
 });
 
+describe('Plugin#loadForms()', ({ before, after, it }) => {
+
+    class UserForm { }
+
+    const readDirectory = sinon.stub().resolves([
+        {
+            name: 'user.form.mjs',
+            isDirectory() {
+                return false;
+            },
+        },
+        {
+            name: 'product',
+            isDirectory() {
+                return true;
+            },
+        },
+        {
+            name: 'user',
+            isDirectory() {
+                return true;
+            },
+        },
+        {
+            name: 'helpers.js',
+            isDirectory() {
+                return false;
+            },
+        },
+    ]);
+
+    const fileSystem = { readDirectory };
+
+    let plugin;
+    let result;
+
+    before(async () => {
+        plugin = new Plugin(fileSystem, DIRECTORY);
+
+        const loadForm = sinon.stub(plugin, 'loadForm');
+        loadForm.onFirstCall().resolves(null);
+        loadForm.onSecondCall().resolves(UserForm);
+
+        result = await plugin.loadForms();
+    });
+
+    after(() => {
+        sinon.restore();
+    });
+
+    it('only loads nested directories', () => {
+        assertEqual(2, plugin.loadForm.callCount);
+        assertEqual(path.join(DIRECTORY, 'forms', 'product'), plugin.loadForm.getCall(0).args[0]);
+        assertEqual(path.join(DIRECTORY, 'forms', 'user'), plugin.loadForm.getCall(1).args[0]);
+    });
+
+    it('uses the plugin name and FormConstructor.name as the key', () => {
+        assertEqual(1, result.size);
+        assert(result.has('application.UserForm'));
+    });
+});
+
 describe('Plugin#loadForm()', ({ before, after, it }) => {
 
     class UserForm { }
@@ -266,6 +390,213 @@ describe('Plugin#loadForm() when class file does not exist', ({ before, after, i
     before(async () => {
         plugin = new Plugin(fileSystem, DIRECTORY);
         result = await plugin.loadForm(directory);
+    });
+
+    after(() => {
+        sinon.restore();
+    });
+
+    it('does not attempt to import a class file', () => {
+        assertEqual(0, importAbsoluteFilepath.callCount);
+    });
+
+    it('returns null', () => {
+        assertEqual(null, result);
+    });
+});
+
+describe('Plugin#loadCollections()', ({ before, after, it }) => {
+
+    class User { }
+
+    class UserCollection {
+        static Model = User;
+    }
+
+    const readDirectory = sinon.stub().resolves([
+        {
+            name: 'user.collection.mjs',
+            isDirectory() {
+                return false;
+            },
+        },
+        {
+            name: 'product',
+            isDirectory() {
+                return true;
+            },
+        },
+        {
+            name: 'user',
+            isDirectory() {
+                return true;
+            },
+        },
+        {
+            name: 'helpers.js',
+            isDirectory() {
+                return false;
+            },
+        },
+    ]);
+
+    const fileSystem = { readDirectory };
+
+    let plugin;
+    let result;
+
+    before(async () => {
+        plugin = new Plugin(fileSystem, DIRECTORY);
+
+        const loadCollection = sinon.stub(plugin, 'loadCollection');
+        loadCollection.onFirstCall().resolves(null);
+        loadCollection.onSecondCall().resolves(UserCollection);
+
+        result = await plugin.loadCollections();
+    });
+
+    after(() => {
+        sinon.restore();
+    });
+
+    it('only loads nested directories', () => {
+        assertEqual(2, plugin.loadCollection.callCount);
+        assertEqual(path.join(DIRECTORY, 'collections', 'product'), plugin.loadCollection.getCall(0).args[0]);
+        assertEqual(path.join(DIRECTORY, 'collections', 'user'), plugin.loadCollection.getCall(1).args[0]);
+    });
+
+    it('uses the plugin name and Model.name as the key', () => {
+        assertEqual(1, result.size);
+        assert(result.has('application.User'));
+    });
+});
+
+describe('Plugin#loadCollection()', ({ before, after, it }) => {
+
+    class User {
+        static name = 'User';
+    }
+
+    class UserCollection {
+        static Model = User;
+    }
+
+    const directory = path.join(DIRECTORY, 'collections', 'user');
+
+    const schema = {
+        type: 'object',
+        properties: {
+            username: { type: 'string' },
+            email: { type: 'string' },
+        },
+    };
+
+    const readDirectory = sinon.stub().resolves([
+        {
+            name: 'user.collection.mjs',
+            isFile() {
+                return true;
+            },
+        },
+        {
+            name: 'user.schema.json',
+            isFile() {
+                return true;
+            },
+        },
+        {
+            name: 'README.md',
+            isFile() {
+                return true;
+            },
+        },
+        {
+            name: 'helpers.js',
+            isFile() {
+                return true;
+            },
+        },
+        {
+            name: 'subdir.collection.js',
+            isFile() {
+                return false;
+            },
+        },
+    ]);
+
+    const readJSONFile = sinon.stub().resolves(schema);
+    const importAbsoluteFilepath = sinon.stub().resolves({ default: UserCollection });
+
+    const fileSystem = {
+        readDirectory,
+        readJSONFile,
+        importAbsoluteFilepath,
+    };
+
+    let plugin;
+    let result;
+
+    before(async () => {
+        plugin = new Plugin(fileSystem, DIRECTORY);
+        result = await plugin.loadCollection(directory);
+    });
+
+    after(() => {
+        sinon.restore();
+    });
+
+    it('only loads JS and schema JSON files', () => {
+        assertEqual(1, readJSONFile.callCount);
+        assertEqual(path.join(directory, 'user.schema.json'), readJSONFile.getCall(0).args[0]);
+
+        assertEqual(1, importAbsoluteFilepath.callCount);
+        assertEqual(path.join(directory, 'user.collection.mjs'), importAbsoluteFilepath.getCall(0).args[0]);
+    });
+
+    it('attaches the schema to the constructor function', () => {
+        assertEqual(schema, UserCollection.schema);
+    });
+
+    it('returns the default export constructor function', () => {
+        assertEqual(UserCollection, result);
+    });
+});
+
+describe('Plugin#loadCollection() when class file does not exist', ({ before, after, it }) => {
+
+    const directory = path.join(DIRECTORY, 'collections', 'incomplete');
+
+    const schema = {
+        type: 'object',
+        properties: {
+            name: { type: 'string' },
+        },
+    };
+
+    const readDirectory = sinon.stub().resolves([
+        {
+            name: 'incomplete.schema.json',
+            isFile() {
+                return true;
+            },
+        },
+    ]);
+
+    const readJSONFile = sinon.stub().resolves(schema);
+    const importAbsoluteFilepath = sinon.stub();
+
+    const fileSystem = {
+        readDirectory,
+        readJSONFile,
+        importAbsoluteFilepath,
+    };
+
+    let plugin;
+    let result;
+
+    before(async () => {
+        plugin = new Plugin(fileSystem, DIRECTORY);
+        result = await plugin.loadCollection(directory);
     });
 
     after(() => {
