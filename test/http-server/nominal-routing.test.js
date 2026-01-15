59c1697596d2bd10fbf09fb2d6bb4550c760b8a2
Kris Walker <kris@kixx.name>
2026-01-15 09:59:56 -0500

    broken: How are we going to address route file locations?
     Changes to be committed:
    	modified:   lib/http-routes-store/http-routes-store.js
    	modified:   test/http-server/nominal-routing.test.js
    	modified:   test/http-server/routing-fixtures/nominal/virtual-hosts.jsonc

--- test/http-server/nominal-routing.test.js
+++ test/http-server/nominal-routing.test.js
@@ -30,6 +30,106 @@ const routesStore = new HttpRoutesStore({
 const router = new HttpRouter();
 
 
+describe('routing with no matching route', ({ before, after, it }) => {
+
+    const url = new URL('http://www.example.com/some-subpage-which-exists');
+
+    const request = createRequest('1', url, {
+        method: 'GET',
+        headers: {
+            accept: '*/*',
+            'user-agent': 'Kixx/Test',
+        },
+    });
+
+    const response = new HttpServerResponse('1');
+
+    const context = createApplicationContext();
+
+    const middleware = new Map();
+    const handlers = new Map();
+    const errorHandlers = new Map();
+
+    const authenticationMiddleware = sinon.spy((_ctx, _req, _res) => {
+        return _res;
+    });
+
+    middleware.set('AuthenticationMiddleware', () => {
+        return authenticationMiddleware;
+    });
+
+    const httpCachingMiddleware = sinon.spy((_ctx, _req, _res) => {
+        return _res;
+    });
+
+    middleware.set('HttpCachingMiddleware', () => {
+        return httpCachingMiddleware;
+    });
+
+    const hyperviewHandler = sinon.spy((_ctx, _req, _res) => {
+        return _res;
+    });
+
+    handlers.set('HyperviewHandler', () => {
+        return hyperviewHandler;
+    });
+
+    const hyperviewErrorHandler = sinon.spy((_ctx, _req, _res/* , _err */) => {
+        return _res;
+    });
+
+    errorHandlers.set('HyperviewErrorHandler', () => {
+        return hyperviewErrorHandler;
+    });
+
+    const routerErrorHandler = sinon.spy();
+
+    let serverResponse;
+
+    before(async () => {
+        router.on('error', routerErrorHandler);
+
+        const vhosts = await routesStore.loadVirtualHosts(middleware, handlers, errorHandlers);
+        router.resetVirtualHosts(vhosts);
+
+        serverResponse = await router.handleHttpRequest(context, request, response);
+    });
+
+    after(() => {
+        router.off('error', routerErrorHandler);
+        sinon.restore();
+    });
+
+    it('does not emit a router error event', () => {
+        assertEqual(0, routerErrorHandler.callCount);
+    });
+
+    it('returns the server response', () => {
+        // The same response should be returned, after being
+        // mutated by the request/response cycle.
+        assertEqual(response, serverResponse);
+    });
+
+    it('calls the inbound authentication middleware', () => {
+        assertEqual(1, authenticationMiddleware.callCount);
+        assert(authenticationMiddleware.calledBefore(hyperviewHandler));
+    });
+
+    it('calls the hyperview page handler', () => {
+        assertEqual(1, hyperviewHandler.callCount);
+        assert(hyperviewHandler.calledBefore(httpCachingMiddleware));
+    });
+
+    it('does not call the hyperview error handler', () => {
+        assertEqual(0, hyperviewErrorHandler.callCount);
+    });
+
+    it('calls the outbound httpCaching middleware', () => {
+        assertEqual(1, httpCachingMiddleware.callCount);
+    });
+});
+
+
 describe('routing with no matching hostname', ({ before, after, it }) => {
 
     const url = new URL('http://localhost:8080');
