93fe634f50652e032a768894037135f79258a041
Kris Walker <kris@kixx.name>
2026-01-25 11:43:41 -0500

    Update the examples for minimatch
     Changes to be committed:
    	modified:   reference/minimatch.js

--- reference/minimatch.js
+++ reference/minimatch.js
@@ -2,6 +2,8 @@ import { minimatch } from '../lib/vendor/mod.js';
 
 const { Minimatch } = minimatch;
 
+/* eslint-disable no-console */
+
 let pattern;
 let glob;
 let pathname;
@@ -15,4 +17,15 @@ glob = new Minimatch(pattern, {
 });
 
 pathname = 'lib/lib/urn-pattern-to-regexp.js';
+console.log('#pattern', pattern, '#pathname', pathname, glob.match(pathname)); // true
+
+pattern = '**/*.js';
+
+glob = new Minimatch(pattern, {
+    // We want to match filenames that begin with a dot:
+    // example `a/**/b` should match `a/.d/b`
+    dot: true,
+});
+
+pathname = 'mod.js';
 console.log('#pattern', pattern, '#pathname', pathname, glob.match(pathname)); // true
