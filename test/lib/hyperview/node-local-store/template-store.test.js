3ca2beb16aed34fe83ef7b5e1ac8342a6afe02b3
Kris Walker <kris@kixx.name>
2026-03-16 16:39:36 -0400

    Export the Hyperview node.js local store.
     Changes to be committed:
    	renamed:    lib/hyperview/local/page-store.js -> lib/hyperview/node-local-store/page-store.js
    	new file:   lib/hyperview/node-local-store/plugin.js
    	renamed:    lib/hyperview/local/static-file-server-store.js -> lib/hyperview/node-local-store/static-file-server-store.js
    	renamed:    lib/hyperview/local/template-store.js -> lib/hyperview/node-local-store/template-store.js
    	modified:   lib/mod.js
    	renamed:    test/lib/hyperview/local/page-store.test.js -> test/lib/hyperview/node-local-store/page-store.test.js
    	renamed:    test/lib/hyperview/local/static-file-server-store.test.js -> test/lib/hyperview/node-local-store/static-file-server-store.test.js
    	renamed:    test/lib/hyperview/local/template-store.test.js -> test/lib/hyperview/node-local-store/template-store.test.js

--- test/lib/hyperview/node-local-store/template-store.test.js
+++ test/lib/hyperview/node-local-store/template-store.test.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'node:url';
 import { describe } from 'kixx-test';
 import { assertEqual, assertArray, assertFunction } from 'kixx-assert';
 import sinon from 'sinon';
-import TemplateStore from '../../../../lib/hyperview/local/template-store.js';
+import TemplateStore from '../../../../lib/hyperview/node-local-store/template-store.js';
 
 
 const THIS_DIR = path.dirname(fileURLToPath(import.meta.url));
