6af146dff8586cdce8af705b2aa8cb1a01d6ad71
Kris Walker <kris@kixx.name>
2026-03-19 14:09:29 -0400

    Create ApplicationBootstrap#createHttpServer()
     Changes to be committed:
    	modified:   lib/bootstrap/application-bootstrap.js
    	modified:   lib/ports/bootstrap.js
    	modified:   test/lib/bootstrap/application-bootstrap.test.js

--- test/lib/bootstrap/application-bootstrap.test.js
+++ test/lib/bootstrap/application-bootstrap.test.js
@@ -1,3 +1,6 @@
+import sinon from 'sinon';
+import { describe } from 'kixx-test';
+import { assertEqual } from 'kixx-assert';
 import ApplicationBootstrap from '../../../lib/bootstrap/application-bootstrap.js';
 import { testPluginLifecycleConformance } from '../../conformance/plugin.js';
 
@@ -24,4 +27,42 @@ const subject = new ApplicationBootstrap({
 
 testPluginLifecycleConformance((plugins, applicationContext) => {
     return subject.loadPlugins(plugins, applicationContext);
+});
+
+describe('ApplicationBootstrap#createHttpServer()', ({ before, it }) => {
+    const mockServer = {};
+    const mockApplicationContext = {};
+    const mockBootstrap = {
+        applicationDirectory: '/app',
+        createConfigStore() {
+            return null;
+        },
+        createHttpRoutesStore() {
+            return null;
+        },
+        getPrintWriter() {
+            return null;
+        },
+        createHttpServer: sinon.fake.returns(mockServer),
+    };
+
+    let result;
+
+    before(() => {
+        const appBootstrap = new ApplicationBootstrap({
+            environment: 'development',
+            bootstrap: mockBootstrap,
+        });
+        result = appBootstrap.createHttpServer(mockApplicationContext, 3000);
+    });
+
+    it('delegates to bootstrap with applicationContext and port', () => {
+        assertEqual(1, mockBootstrap.createHttpServer.callCount);
+        assertEqual(mockApplicationContext, mockBootstrap.createHttpServer.firstCall.args[0]);
+        assertEqual(3000, mockBootstrap.createHttpServer.firstCall.args[1]);
+    });
+
+    it('returns the server from bootstrap', () => {
+        assertEqual(mockServer, result);
+    });
 });
