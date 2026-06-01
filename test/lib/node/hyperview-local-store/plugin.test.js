b590ccd41b79d95ce50c9519fc67911495e0495e
Kris Walker <kris@kixx.name>
2026-06-01 18:55:04 -0400

    Remove tests
    We have new tests from a month's worth of app development that we are
    going to move back into place on the main branch

--- test/lib/node/hyperview-local-store/plugin.test.js
+++
@@ -1,7 +0,0 @@
-import * as plugin from '../../../../lib/node/hyperview-local-store/plugin.js';
-import { testPluginConformance } from '../../../conformance/plugin.js';
-
-
-// The plugin module exports named functions register() and initialize() directly,
-// matching the Plugin port shape.
-testPluginConformance(() => plugin);
