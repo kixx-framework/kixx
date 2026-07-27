fe8296d7d7eb4a4f8ef8d176cd8f4b18a10d0017
Kris Walker <kris@kixx.name>
2026-07-27 09:26:04 -0400

    New e2e test for home page
     Changes to be committed:
    	new file:   test/end-to-end/001-sanity-checks/001-home-page.test.js

---
+++ test/end-to-end/001-sanity-checks/001-home-page.test.js
@@ -0,0 +1,40 @@
+import { FastHTMLParser } from 'fast-html-dom-parser';
+import { describe } from 'kixx-test';
+import {
+    assert,
+    assertEqual,
+    assertMatches,
+} from 'kixx-assert';
+import { getBaseUrl } from '../test-helpers/lib.js';
+import validateHtml from '../test-helpers/validate-html.js';
+
+
+describe('home page HTML', ({ before, it }) => {
+
+    let url;
+    let response;
+    let body;
+
+    before(async () => {
+        url = new URL(getBaseUrl());
+        response = await fetch(url);
+        body = await response.text();
+    });
+
+    it('returns the home page HTML', () => {
+        assert(response);
+        assertEqual(200, response.status);
+        assertEqual(url.href, response.url);
+        assertEqual('text/html; charset=utf-8', response.headers.get('content-type'));
+        // Match a sample of the HTML document, just to be sure there is something there.
+        assertMatches('<!doctype html>', body.slice(0, 50));
+    });
+
+    it('renders valid HTML', async () => {
+        // The response body is a full HTML document, so it can be validated directly.
+        await validateHtml(body);
+        const document = new FastHTMLParser(body);
+        const [ bodyNode ] = document.getElementsByTagName('body');
+        assertEqual('BODY', bodyNode.nodeName);
+    });
+});
