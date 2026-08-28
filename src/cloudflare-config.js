219c66ad99462e0778c1312d2974fa7c0bc5697d
Kris Walker <kris@kixx.name>
2026-08-28 17:21:55 -0400

    Update cloudflare config for new worker
    	modified:   src/cloudflare-config.js

--- src/cloudflare-config.js
+++ src/cloudflare-config.js
@@ -1,123 +1,22 @@
 export default {
-    name: 'kixx-app',
+    name: 'kixx-test-app',
 
     environments: {
-        development: {
-            HYPERVIEW: {
-                useTemplateCache: false,
-                usePageCache: false,
-                allowJsonResponse: true,
-                pageCacheReadTtlSeconds: 0,
-                pageCacheExpirationSeconds: 0,
-            },
-            SECRET_ENCRYPTION: {
-                PBKDF2_ITERATIONS: 50000,
-            },
-            RATE_LIMIT: {
-                ADMIN_LOGIN: {
-                    maxFailures: 5,
-                    windowSeconds: 900,
-                    cooldownSeconds: 900,
-                },
-                ADMIN_SIGNUP: {
-                    maxFailures: 10,
-                    windowSeconds: 900,
-                    cooldownSeconds: 900,
-                },
-                ADMIN_INVITE: {
-                    maxFailures: 3,
-                    windowSeconds: 900,
-                    cooldownSeconds: 3600,
-                },
-            },
-            DOCUMENT_STORE: {
-                type: 'd1',
-                bindingName: 'DOCUMENT_STORE',
-                databaseId: 'a-d1-database-uuid',
-            },
-            KEY_VALUE_STORE: {
-                type: 'kv_namespace',
-                bindingName: 'KEY_VALUE_STORE',
-                namespaceId: 'a-kv-namespace-uuid',
-            },
-            OBJECT_STORE: {
-                type: 'r2_bucket',
-                buckets: {
-                    // This is just an example of a configured bucket. Buckets
-                    // will need to be configured before they are available.
-                    files: {
-                        bucketName: 'local-development-files',
-                        bindingName: 'OBJECT_STORE_FILES',
-                    },
-                },
-            },
-            CONTENT_STORE: {
-                // The content-store adapter clamps blob cache TTLs to at least
-                // 60 seconds. Blob keys carry the content hash, so caching them
-                // never hides an edit.
-                blobReadCacheTtlSeconds: 60,
-                indexCacheTtlSeconds: 0,
-                kvBindingName: 'CA_STORE_KV_STORE',
-                durableObjectBindingName: 'CA_STORE_DURABLE_OBJECT',
-            },
-        },
-        staging: {
-            HYPERVIEW: {
-                useTemplateCache: true,
-                usePageCache: true,
-                allowJsonResponse: true,
-                pageCacheReadTtlSeconds: 60 * 5,
-                pageCacheExpirationSeconds: 60 * 12,
-            },
-            SECRET_ENCRYPTION: {
-                PBKDF2_ITERATIONS: 50000,
-            },
-            RATE_LIMIT: {
-                ADMIN_LOGIN: {
-                    maxFailures: 5,
-                    windowSeconds: 900,
-                    cooldownSeconds: 900,
-                },
-                ADMIN_SIGNUP: {
-                    maxFailures: 10,
-                    windowSeconds: 900,
-                    cooldownSeconds: 900,
-                },
-                ADMIN_INVITE: {
-                    maxFailures: 3,
-                    windowSeconds: 900,
-                    cooldownSeconds: 3600,
-                },
-            },
-            DOCUMENT_STORE: {
-                type: 'd1',
-                bindingName: 'DOCUMENT_STORE',
-                databaseId: 'a-d1-database-uuid',
-            },
-            KEY_VALUE_STORE: {
-                type: 'kv_namespace',
-                bindingName: 'KEY_VALUE_STORE',
-                namespaceId: 'a-kv-namespace-uuid',
-            },
-            OBJECT_STORE: {
-                type: 'r2_bucket',
-                buckets: {
-                    // This is just an example of a configured bucket. Buckets
-                    // will need to be configured before they are available.
-                    files: {
-                        bucketName: 'kixx-app-staging-files',
-                        bindingName: 'OBJECT_STORE_FILES',
-                    },
-                },
-            },
-            CONTENT_STORE: {
-                blobReadCacheTtlSeconds: 60 * 60 * 24,
-                indexCacheTtlSeconds: 10,
-                kvBindingName: 'CA_STORE_KV_STORE',
-                durableObjectBindingName: 'CA_STORE_DURABLE_OBJECT',
-            },
-        },
         production: {
+            WORKER: {
+                name: 'kixx-test-app',
+                logpush: false,
+                observability: {
+                    enabled: true,
+                    head_sampling_rate: 1,
+                    logs: {
+                        enabled: true,
+                        invocation_logs: true,
+                        persist: true,
+                        head_sampling_rate: 1,
+                    },
+                },
+            },
             HYPERVIEW: {
                 useTemplateCache: true,
                 usePageCache: true,
@@ -160,16 +59,17 @@ export default {
                 buckets: {
                     // This is just an example of a configured bucket. Buckets
                     // will need to be configured before they are available.
-                    files: {
-                        bucketName: 'kixx-app-production-files',
-                        bindingName: 'OBJECT_STORE_FILES',
-                    },
+                    // files: {
+                    //     bucketName: 'kixx-app-production-files',
+                    //     bindingName: 'OBJECT_STORE_FILES',
+                    // },
                 },
             },
             CONTENT_STORE: {
                 blobReadCacheTtlSeconds: 60 * 60 * 36,
                 indexCacheTtlSeconds: 10,
                 kvBindingName: 'CA_STORE_KV_STORE',
+                kvNamespaceId: 'a-kv-namespace-uuid',
                 durableObjectBindingName: 'CA_STORE_DURABLE_OBJECT',
             },
         },
