a5ab37c00a45d5db97ba829e37e82a3a76c8e253
Kris Walker <kris@kixx.name>
2026-09-03 14:01:26 -0400

    Remove the Node.js staging environment
    	modified:   src/node-config.js

--- src/node-config.js
+++ src/node-config.js
@@ -53,53 +53,6 @@ export default {
                 emailsDirectory: './emails',
             },
         },
-        staging: {
-            LOGGER: {
-                level: 'info',
-            },
-            HYPERVIEW: {
-                useTemplateCache: true,
-                usePageCache: true,
-                allowJsonResponse: true,
-                pageCacheReadTtlSeconds: 60 * 5,
-                pageCacheExpirationSeconds: 60 * 12,
-            },
-            SECRET_ENCRYPTION: {
-                PBKDF2_ITERATIONS: 50000,
-            },
-            RATE_LIMIT: {
-                ADMIN_LOGIN: {
-                    maxFailures: 5,
-                    windowSeconds: 900,
-                    cooldownSeconds: 900,
-                },
-                ADMIN_SIGNUP: {
-                    maxFailures: 10,
-                    windowSeconds: 900,
-                    cooldownSeconds: 900,
-                },
-                ADMIN_INVITE: {
-                    maxFailures: 3,
-                    windowSeconds: 900,
-                    cooldownSeconds: 3600,
-                },
-            },
-            DOCUMENT_STORE: {
-                path: '../data/nodejs_app/document_store.sqlite',
-            },
-            KEY_VALUE_STORE: {
-                path: '../data/nodejs_app/key_value_store.sqlite',
-            },
-            OBJECT_STORE: {
-                path: '../data/nodejs_app/object_store',
-                buckets: {
-                    uploads: {},
-                },
-            },
-            CONTENT_STORE: {
-                rootDirectory: '../data/nodejs_app/content_store',
-            },
-        },
         local: {
             LOGGER: {
                 level: 'info',
